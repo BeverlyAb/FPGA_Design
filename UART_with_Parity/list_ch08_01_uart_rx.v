e614b1770e897f6089a4e83437dc4acd5edd1543
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2017-04-03 11:42:04 -0700

    Update list_ch08_01_uart_rx.v

--- UART_with_Parity/list_ch08_01_uart_rx.v
+++ UART_with_Parity/list_ch08_01_uart_rx.v
@@ -8,7 +8,7 @@ module uart_rx
     input wire clk, reset,
     input wire rx, s_tick,
     output reg rx_done_tick,
-	 output reg error,		//1 - parity mismatch, 0 parity match
+    output reg error,		//1 - parity mismatch, 0 parity match
     output wire [7:0] dout
    );
 
@@ -18,14 +18,14 @@ module uart_rx
       START = 3'b001,
       DATA  = 3'b010,
       PARITY = 3'b011,
-		STOP  = 3'b100;
+      STOP  = 3'b100;
 		
    // signal declaration
    reg [2:0] state_reg, state_next;
    reg [3:0] s_reg, s_next;
    reg [2:0] n_reg, n_next;
    reg [7:0] b_reg, b_next;
-	reg p_reg, p_next;	//rolling values, between 0 and 1
+   reg p_reg, p_next;	//rolling values, between 0 and 1
    // body
    // FSMD state & DATA registers
    always @(posedge clk, posedge reset)
@@ -80,24 +80,24 @@ module uart_rx
                   begin
                      s_next = 0;
                      b_next = {rx, b_reg[7:1]};
-							p_next = (rx) ? p_reg + 1: p_reg;
+		     p_next = (rx) ? p_reg + 1: p_reg;
                      if (n_reg==(DBIT-1))
                         state_next = PARITY ;
-							else
+		     else
                         n_next = n_reg + 1;
                    end
                else
                   s_next = s_reg + 1;
-			PARITY:
-				if(s_tick)
-					if(s_reg ==15)
-						begin
-							s_next = 0;
-							state_next = STOP;
-							error = (p_reg  == rx) ? 0 : 1; //1 - parity mismatch, 0 parity match
-						end
-					else
-						s_next = s_reg + 1;
+         PARITY:
+	     if(s_tick)
+		if(s_reg ==15)
+		     begin
+		        s_next = 0;
+			state_next = STOP;
+			error = (p_reg  == rx) ? 0 : 1; //1 - parity mismatch, 0 parity match
+		     end
+		else
+		     s_next = s_reg + 1;
          STOP:
             if (s_tick)
                if (s_reg==(SB_TICK-1))
@@ -107,14 +107,14 @@ module uart_rx
                   end
                else
                   s_next = s_reg + 1;
-			default: begin
-				state_next = IDLE;
-				rx_done_tick = 1'b0;
-				s_next = 0;
-            n_next = 0;
-            b_next = 0;
-				p_next = 0;
-			end
+	default: begin
+	     state_next = IDLE;
+	     rx_done_tick = 1'b0;
+	     s_next = 0;
+             n_next = 0;
+             b_next = 0;
+	     p_next = 0;
+	end
       endcase
    end
    // output
